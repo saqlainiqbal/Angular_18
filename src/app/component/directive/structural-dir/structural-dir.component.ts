43cb1e5c98bb420b29a810d8c18b796201f80a37
Saqlain Iqbal <saqlainameer011@gmail.com>
2025-02-05 00:06:45 +0500

    Router navigateByUrl.

--- src/app/component/directive/structural-dir/structural-dir.component.ts
+++ src/app/component/directive/structural-dir/structural-dir.component.ts
@@ -1,20 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-structural-dir',
   imports: [CommonModule , FormsModule],
   templateUrl: './structural-dir.component.html',
+  
   styleUrl: './structural-dir.component.css'
 })
 export class StructuralDirComponent {
+
+  constructor( private router: Router) { }
+  navigatetoDataBinding(){
+    this.router.navigateByUrl("/Data-Binding");
+  }
   isDiv1Visible: boolean = true;
   isDiv2Visible: boolean = true;
   num1: string = '';
   num2: string = '';
   isCheckBoxChecked: boolean = true;
-  
+
 
   showDiv1(){
     this.isDiv1Visible = true;
