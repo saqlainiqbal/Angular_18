7832581dc9489a9d294b53ecd1aaded93220856d
Saqlain Iqbal <saqlainameer011@gmail.com>
2025-02-04 23:22:16 +0500

    Configure Routing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { AddEmployeeComponent } from "./component/add-employee/add-employee.component";
-import { ListEmployeeComponent } from './component/list-employee/list-employee.component';
-import { DataBindingComponent } from "./component/data-binding/data-binding.component";
-import { StructuralDirComponent } from "./component/directive/structural-dir/structural-dir.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ AddEmployeeComponent, ListEmployeeComponent, DataBindingComponent, StructuralDirComponent],
+  imports: [RouterLink , RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AddEmployeeComponent } from './component/add-employee/add-employee.component';
+import { DataBindingComponent } from './component/data-binding/data-binding.component';
+import { ListEmployeeComponent } from './component/list-employee/list-employee.component';
+import { StructuralDirComponent } from './component/directive/structural-dir/structural-dir.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'Add-Employee',
+        component: AddEmployeeComponent
+    },
+    {
+        path: 'Data-Binding',
+        component: DataBindingComponent
+    },
+    {
+        path: 'List-Employee',
+        component: ListEmployeeComponent
+    },
+    {
+        path: 'Structural-dir',
+        component: StructuralDirComponent
+    }
+
+];
